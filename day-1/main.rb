815467effa8ed9e52b3396021d2802870fd08686
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-10 15:20:30 +0100

    doc: day-1

--- day-1/main.rb
+++ day-1/main.rb
@@ -1,30 +1,33 @@
-# part 1
-
-file = File.open("input.txt")
-numbers = file.readlines.map(&:chomp)
-numbers.map!(&:to_i)
-
-
+# this method counts the number of times a number is bigger than the one at the position before it
 def get_num_increases(numbers)
   counter = 0
   numbers.each_with_index do |number, index|
-    break if index == numbers.length - 1
-    # puts numbers[index + 1]
+    break if index == numbers.length - 1 # leaves the loop in case there is no number after the current number
+
     counter += 1 if number < numbers[index + 1]
   end
   counter
 end
 
-answer_1 = get_num_increases(numbers)
+def get_sums_array(numbers)
+  sums_array = []
 
-# part 2
-sums_array = []
+  numbers.each_with_index do |number, index|
+    break if index == numbers.length - 2 # breaks loop if there are no more numbers available
 
-numbers.each_with_index do |number, index|
-  break if index == numbers.length - 2
-
-  sums_array << number + numbers[index + 1] + numbers[index + 2]
+    sums_array << number + numbers[index + 1] + numbers[index + 2]
+  end
 end
 
-answer_2 = get_num_increases(sums_array)
+# read file and store data in integer array
+file = File.open("input.txt")
+numbers = file.readlines.map(&:chomp)
+numbers.map!(&:to_i)
+
+# part 1
+answer_1 = get_num_increases(numbers)
+p answer_1
+
+# part 2
+answer_2 = get_num_increases(get_sums_array(numbers))
 p answer_2
