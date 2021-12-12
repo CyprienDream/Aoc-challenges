4daaea0707441aaf223c652ae969b267902cc909
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-12 16:31:00 +0100

    solve day-12 + doc

--- day-12/main.rb
+++ day-12/main.rb
@@ -1,3 +1,4 @@
+# store input into hash with cave as keys and array of adjacent caves as values
 cavemap = Hash.new{|h,k|h[k]=[]}
 input = File.open('input.txt').read.split.each do |line|
     a, b = line.split('-')
@@ -5,17 +6,24 @@ input = File.open('input.txt').read.split.each do |line|
     cavemap[b] << a
 end
 
-def small?(cave) = cave.match?(/^[a-z]+/)
+# returns true if the cave is small
+def small?(cave)
+  cave.match?(/^[a-z]+/)
+end
 
+# this method uses recursion to calculate the number of possible paths
 def countpaths(graph, doubled, visited, from, to)
-    return 1 if from == to
-    if visited.include?(from)
-        return 0 if doubled || from == 'start'
-        doubled = true
-    end
-    graph[from].sum do |dest|
-        countpaths(graph, doubled, small?(from) ? visited + [from] : visited, dest, to)
-    end
+  # add 1 to total whenever end cave is reached
+  return 1 if from == to
+  # skips to next path if cave is small and has already been visited (part 1) or if the cave is start (part 2)
+  if visited.include?(from)
+    return 0 if doubled || from == 'start'
+    doubled = true
+  end
+  # iterate through the array of adjacent caves of all the adjacent caves of start adn sum up the result
+  graph[from].sum do |dest|
+    countpaths(graph, doubled, small?(from) ? visited + [from] : visited, dest, to)
+  end
 end
 
 # Part 1
