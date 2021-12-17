a94f1b5b8c6bd6c45cd1126bbf29bccd63b7774d
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-17 18:05:33 +0100

    clean up day 17

--- day-17/main.rb
+++
@@ -1,4 +0,0 @@
-xrange, yrange = open("input").read.split(",").map{eval(_1.split("=").last)}
-
-def triangular(n) = n * (n + 1) / 2
-def loc(init_velocity, steps) = steps * ((init_velocity + (init_velocity - steps + 1)) / 2.0)
